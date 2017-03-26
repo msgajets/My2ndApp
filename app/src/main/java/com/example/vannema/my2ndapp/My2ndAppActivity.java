212671f3e02d0c3a00e67d03bdf79b4707f7a789
gajets <msgajets@gmail.com>
2017-03-26 16:41:15 -0400

    Add not staged .idea/misc.xml

---
+++ app/src/main/java/com/example/vannema/my2ndapp/My2ndAppActivity.java
@@ -0,0 +1,13 @@
+package com.example.vannema.my2ndapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class My2ndAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my2nd_app);
+    }
+}
